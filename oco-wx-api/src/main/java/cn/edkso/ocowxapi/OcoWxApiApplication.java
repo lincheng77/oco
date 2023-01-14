d51ceea7b8c5c21f2be36b5b952f52f036bbcf28
lincheng77 <1498047434@qq.com>
2023-01-15 01:34:23 +0800

    V1-在线办公小程序初始化后端

---
+++ oco-wx-api/src/main/java/cn/edkso/ocowxapi/OcoWxApiApplication.java
@@ -0,0 +1,13 @@
+package cn.edkso.ocowxapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OcoWxApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OcoWxApiApplication.class, args);
+    }
+
+}
---
+++ oco-wx-api/src/test/java/cn/edkso/ocowxapi/OcoWxApiApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.edkso.ocowxapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OcoWxApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
