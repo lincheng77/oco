0d6f514f25f5cc752f3a04888800c6060d2b835f
lincheng77 <1498047434@qq.com>
2023-01-15 21:10:27 +0800

    V1.2.0 封装SwaggerConfig.java

---
+++ oco-wx-api/src/main/java/cn/edkso/oco/config/SwaggerConfig.java
@@ -0,0 +1,59 @@
+package cn.edkso.oco.config;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.SecurityReference;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
+import springfox.documentation.spring.web.plugins.ApiSelectorBuilder;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+    @Bean
+    public Docket createRestApi() {
+
+        /** swagger 基础信息*/
+        Docket docket = new Docket(DocumentationType.SWAGGER_2);
+        ApiInfoBuilder builder = new ApiInfoBuilder();
+        builder.title("林城在线办公系统");
+        ApiInfo info = builder.build();
+        docket.apiInfo(info);
+
+        /** swagger 作用范围 （any， 使用ApiOperation注解的地方）*/
+        ApiSelectorBuilder selectorBuilder = docket.select();
+        selectorBuilder.paths(PathSelectors.any());
+        selectorBuilder.apis(RequestHandlerSelectors.withMethodAnnotation(ApiOperation.class));
+        docket = selectorBuilder.build();
+
+        /** swagger 融合jwt，接收令牌配置信息*/
+        ApiKey apiKey = new ApiKey("token", "token", "header");
+        List<ApiKey> apiKeyList = new ArrayList<>();
+        apiKeyList.add(apiKey);
+        docket.securitySchemes(apiKeyList);
+
+        /** swagger 融合jwt，接收令牌配置信息 -> 作用范围*/
+        AuthorizationScope scope = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] scopes = {scope};
+        SecurityReference reference = new SecurityReference("token", scopes);
+        List refList = new ArrayList();
+        refList.add(reference);
+        SecurityContext context = SecurityContext.builder().securityReferences(refList).build();
+        List cxtList = new ArrayList();
+        cxtList.add(context);
+        docket.securityContexts(cxtList);
+
+        return docket;
+    }
+}
