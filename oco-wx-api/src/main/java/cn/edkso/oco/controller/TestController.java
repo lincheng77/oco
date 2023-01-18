55130d568542e223db28c1d47c6d308c7d5f643e
lincheng77 <1498047434@qq.com>
2023-01-18 15:10:53 +0800

    V1.2.1 测试Swagger2访问请求(增加参数校验)

--- oco-wx-api/src/main/java/cn/edkso/oco/controller/TestController.java
+++ oco-wx-api/src/main/java/cn/edkso/oco/controller/TestController.java
@@ -2,11 +2,15 @@ package cn.edkso.oco.controller;
 
 
 import cn.edkso.oco.common.util.R;
+import cn.edkso.oco.controller.form.TestSayHelloForm;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 
 @RestController
@@ -15,8 +19,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @PostMapping("/sayHello")
     @ApiOperation("最简单的测试方法")
-    public R sayHello() {
-        return R.ok().put("message", "HelloWorld");
+    public R sayHello(@Valid @RequestBody TestSayHelloForm testSayHelloForm) {
+        return R.ok().put("message", "Hello" + testSayHelloForm.getName());
     }
 
 
---
+++ oco-wx-api/src/main/java/cn/edkso/oco/controller/form/TestSayHelloForm.java
@@ -0,0 +1,18 @@
+package cn.edkso.oco.controller.form;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
+@ApiModel
+@Data
+public class TestSayHelloForm {
+    @NotBlank
+    //u4e00 - u9fa5 表示汉子  2-15个
+    @Pattern(regexp = "^[\\u4e00-\\u9fa5]{2,15}$",message = "不符合正则表达式")
+    @ApiModelProperty("姓名")
+    private String name;
+}
