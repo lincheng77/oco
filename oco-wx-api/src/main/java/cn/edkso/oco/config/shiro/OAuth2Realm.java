4228b243d671107945306cb306a874a9504ceb6d
lincheng77-macbookair <1498047434@qq.com>
2023-01-28 14:36:04 +0800

    V1.3.6 创建OAuth2Realm类

---
+++ oco-wx-api/src/main/java/cn/edkso/oco/config/shiro/OAuth2Realm.java
@@ -0,0 +1,52 @@
+package cn.edkso.oco.config.shiro;
+
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.AuthenticationInfo;
+import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.authc.SimpleAuthenticationInfo;
+import org.apache.shiro.authz.AuthorizationInfo;
+import org.apache.shiro.authz.SimpleAuthorizationInfo;
+import org.apache.shiro.realm.AuthorizingRealm;
+import org.apache.shiro.subject.PrincipalCollection;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class OAuth2Realm extends AuthorizingRealm {
+
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    @Override
+    public boolean supports(AuthenticationToken token) {
+        return token instanceof OAuth2Token;
+    }
+
+
+    /**
+     * 授权（验证权限时调用）
+     * @param principalCollection
+     * @return
+     */
+    @Override
+    protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principalCollection) {
+        SimpleAuthorizationInfo info = new SimpleAuthorizationInfo();
+        //TODO 查询用户的权限列表
+        //TODO 把权限列表添加到info对象中
+        return null;
+    }
+
+    /**
+     * 认证（登录时调用）
+     * @param authenticationToken
+     * @return
+     * @throws AuthenticationException
+     */
+    @Override
+    protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken authenticationToken) throws AuthenticationException {
+        //TODO 从令牌中获取userid，然后检测账户是否被冻结
+        SimpleAuthenticationInfo info = new SimpleAuthenticationInfo();
+        //TODO 往info对象中添加用户信息，token字符串
+        return info;
+    }
+}
