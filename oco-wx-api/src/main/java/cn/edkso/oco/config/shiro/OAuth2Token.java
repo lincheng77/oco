91d731aaee68a3feb4e8dc56e634b0ca70c21fd1
lincheng77-macbookair <1498047434@qq.com>
2023-01-28 10:26:17 +0800

    V1.3.5 把令牌封装成认证对象OAuth2Token.java

---
+++ oco-wx-api/src/main/java/cn/edkso/oco/config/shiro/OAuth2Token.java
@@ -0,0 +1,22 @@
+package cn.edkso.oco.config.shiro;
+
+import org.apache.shiro.authc.AuthenticationToken;
+
+public class OAuth2Token implements AuthenticationToken {
+
+    private String token;
+
+    public OAuth2Token(String token) {
+        this.token = token;
+    }
+
+    @Override
+    public Object getPrincipal() {
+        return this.token;
+    }
+
+    @Override
+    public Object getCredentials() {
+        return this.token;
+    }
+}
