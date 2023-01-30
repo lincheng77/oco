092d1393fec024f0e66f4078d06a5f195960c542
lincheng77-macbookair <1498047434@qq.com>
2023-01-30 14:16:56 +0800

    V1.4.1 创建style.less、static静态资源、登录页面

--- oco-wx/unpackage/dist/dev/mp-weixin/app.js
+++ oco-wx/unpackage/dist/dev/mp-weixin/app.js
@@ -2,6 +2,7 @@
 Object.defineProperties(exports, { __esModule: { value: true }, [Symbol.toStringTag]: { value: "Module" } });
 const common_vendor = require("./common/vendor.js");
 if (!Math) {
+  "./pages/login/login.js";
   "./pages/index/index.js";
 }
 const _sfc_main = {
@@ -15,7 +16,7 @@ const _sfc_main = {
     console.log("App Hide");
   }
 };
-const App = /* @__PURE__ */ common_vendor._export_sfc(_sfc_main, [["__file", "/Users/dingmengdi/Developer/code/applet/learn/uni-app\u4ECE\u5165\u95E8\u5230\u8FDB\u9636\u7CFB\u7EDF\u5B8C\u6210\u9879\u76EE\u5B9E\u6218/oco-wx/App.vue"]]);
+const App = /* @__PURE__ */ common_vendor._export_sfc(_sfc_main, [["__file", "/Users/dingmengdi/Developer/code/applet/learn/\u5728\u7EBF\u534F\u540C\u529E\u516C\u5C0F\u7A0B\u5E8F\u5F00\u53D1 \u5168\u6808\u5F0F\u9879\u76EE\u5B9E\u6218/oco/oco-wx/App.vue"]]);
 function createApp() {
   const app = common_vendor.createSSRApp(App);
   return {
--- oco-wx/unpackage/dist/dev/mp-weixin/pages/index/index.js
+++ oco-wx/unpackage/dist/dev/mp-weixin/pages/index/index.js
@@ -15,5 +15,5 @@ function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
     a: common_vendor.t($data.title)
   };
 }
-const MiniProgramPage = /* @__PURE__ */ common_vendor._export_sfc(_sfc_main, [["render", _sfc_render], ["__file", "/Users/dingmengdi/Developer/code/applet/learn/uni-app\u4ECE\u5165\u95E8\u5230\u8FDB\u9636\u7CFB\u7EDF\u5B8C\u6210\u9879\u76EE\u5B9E\u6218/oco-wx/pages/index/index.vue"]]);
+const MiniProgramPage = /* @__PURE__ */ common_vendor._export_sfc(_sfc_main, [["render", _sfc_render], ["__file", "/Users/dingmengdi/Developer/code/applet/learn/\u5728\u7EBF\u534F\u540C\u529E\u516C\u5C0F\u7A0B\u5E8F\u5F00\u53D1 \u5168\u6808\u5F0F\u9879\u76EE\u5B9E\u6218/oco/oco-wx/pages/index/index.vue"]]);
 wx.createPage(MiniProgramPage);
